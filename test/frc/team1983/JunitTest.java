f6be68988be184bfa82f3090814abb67538757f6
Erik Pratt <erikjensenpratt@gmail.com>
2019-01-12 11:17:47 -0800

    Removed .idea/ and reinstated test folder

---
+++ test/frc/team1983/JunitTest.java
@@ -0,0 +1,12 @@
+package frc.team1983;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class JunitTest {
+    @Test
+    public void testJunit()
+    {
+        Assert.assertTrue(true);
+    }
+}
