7dc2cdca38673031e27d932884082ecfc00c9d97
Klavla <erikjensenpratt@gmail.com>
2019-01-26 14:16:46 -0800

    Added a mockito test

---
+++ test/frc/team1983/utilities/UT_Mockito.java
@@ -0,0 +1,37 @@
+package frc.team1983.utilities;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class UT_Mockito
+{
+    @Mock
+    private ClassToBeMocked mockedClass;
+
+    @Before
+    public void setup()
+    {
+        initMocks(this);
+    }
+
+    @Test
+    public void mockitoOverridesActualMethodCall()
+    {
+        when(mockedClass.returnFalse()).thenReturn(true);
+
+        Assert.assertTrue(mockedClass.returnFalse());
+    }
+
+    class ClassToBeMocked
+    {
+        boolean returnFalse()
+        {
+            return false;
+        }
+    }
+}
