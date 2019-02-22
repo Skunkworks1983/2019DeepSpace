832f7cb409dd9da54c184acd807e078fd5c2e24b
Joseph Nwizugbo <josephnwz@hotmail.com>
2019-02-21 20:59:36 -0800

    sub climb CommandGroups

--- src/frc/team1983/commands/crossSubsystem/Collapse.java
+++ src/frc/team1983/commands/crossSubsystem/Collapse.java
@@ -1,32 +1,31 @@
 package frc.team1983.commands.crossSubsystem;
 
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.team1983.Robot;
 import frc.team1983.commands.collector.SetCollectorFolded;
 import frc.team1983.commands.elevator.SetElevatorPosition;
 import frc.team1983.subsystems.Collector;
+import frc.team1983.subsystems.Drivebase;
 import frc.team1983.subsystems.Elevator;
 
-public class ClimbSetup extends CommandGroup
-{
+public class Collapse extends CommandGroup {
+
     private Elevator elevator;
     private Collector collector;
+    private Drivebase drivebase;
 
-    public ClimbSetup()
+
+    public Collapse()
     {
-        elevator = Robot.getInstance().getElevator();
-        collector = Robot.getInstance().getCollector();
+        Elevator elevator = Robot.getInstance().getElevator();
+        Collector collector = Robot.getInstance().getCollector();
+        Drivebase drivebase = Robot.getInstance().getDrivebase();
 
-        //Raises elevator for collector deploy clearance.
         addSequential(new SetElevatorPosition(elevator, 22));
-
-        //Unfolds collector to prepare for "hooking" onto hab.
-        //*At what distance from the hab should we unfold?*
-        addSequential(new SetCollectorFolded(collector, false));
-
-        //Lowers Elevator back to original height
+        addSequential(new SetCollectorFolded(collector, true));
         addSequential(new SetElevatorPosition(elevator, 0));
+        drivebase.setBrake(true);
     }
+
 
 }
---
+++ src/frc/team1983/commands/crossSubsystem/PullForward.java
@@ -0,0 +1,11 @@
+package frc.team1983.commands.crossSubsystem;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class PullForward extends CommandGroup
+{
+    public PullForward()
+    {
+
+    }
+}
