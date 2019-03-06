ed0a9b94d6c78b4411ca834c7bcfd9b8921bcdc0
Dillon Charles <dilloncharles4@gmail.com>
2019-03-05 19:15:13 -0800

    Generalized climb command

--- src/frc/team1983/commands/climber/Climb.java
+++ src/frc/team1983/commands/climber/Climb.java
@@ -1,9 +1,7 @@
 package frc.team1983.commands.climber;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.team1983.Robot;
-import frc.team1983.commands.collector.SetCollectorAngle;
 import frc.team1983.services.OI;
 import frc.team1983.subsystems.Climber;
 import frc.team1983.subsystems.Collector;
@@ -11,47 +9,51 @@ import frc.team1983.subsystems.Drivebase;
 import frc.team1983.utilities.motors.ControlMode;
 import frc.team1983.utilities.sensors.Gyro;
 
-public class ClimbLevelTwo extends Command
+public class Climb extends Command
 {
     private Climber climber;
     private Collector collector;
     private Drivebase drivebase;
     private Gyro gyro;
-    private  double throttle;
+    private double climbHeight;
+    private double driveHeight;
 
-    public ClimbLevelTwo(Climber climber, Collector collector, Drivebase drivebase, Gyro gyro)
+    public Climb(Climber climber, Collector collector, Drivebase drivebase, Gyro gyro, double climbHeight, double driveHeight)
     {
         this.climber = climber;
         this.collector = collector;
         this.drivebase = drivebase;
         this.gyro = gyro;
+        this.climbHeight = climbHeight;
+        this.driveHeight = driveHeight;
     }
 
-    public ClimbLevelTwo()
+    public Climb(double climbHeight, double driveHeight)
     {
-        this(Robot.getInstance().getClimber(), Robot.getInstance().getCollector(), Robot.getInstance().getDrivebase(), Robot.getInstance().getGyro());
+        this(Robot.getInstance().getClimber(), Robot.getInstance().getCollector(), Robot.getInstance().getDrivebase(), Robot.getInstance().getGyro(), climbHeight, driveHeight);
     }
 
     @Override
     public void initialize()
     {
         Robot.getInstance().getCollector().climbing = true;
-        climber.set(ControlMode.Position, -14);
         gyro.setPitch(0);
+        climber.set(ControlMode.Position, climbHeight);
     }
 
     @Override
     protected void execute()
     {
-        throttle = gyro.getPitch() / 3;
+        double throttle = gyro.getPitch() / 4;
 
-        if (collector.getAngle() < 120 && throttle < 0) throttle = 0;
+        if (collector.getAngle() < 90 && throttle < 0) throttle = 0;
         if (collector.getAngle() > 190 && throttle > 0) throttle = 0;
         collector.setWristThrottle(throttle);
 
-        collector.setRollerThrottle(1);
+        collector.setRollerThrottle(climber.getPosition() > driveHeight ? 1 : 0.4);
 
-        if(climber.getPosition() < -8) drivebase.set(ControlMode.Throttle, 0.2);
+        if(climber.getPosition() <= driveHeight)
+            drivebase.set(ControlMode.Throttle, 0.2);
     }
 
     @Override
