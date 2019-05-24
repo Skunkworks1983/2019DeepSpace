68933ba37719285e08dfa7c5e479aed1808289b1
KaiNakamura <kainakamura.rahs@gmail.com>
2019-05-23 19:49:55 -0700

    working, maybe not tuned

--- src/frc/team1983/commands/drivebase/SteeringDrive.java
+++ src/frc/team1983/commands/drivebase/SteeringDrive.java
@@ -1,6 +1,5 @@
 package frc.team1983.commands.drivebase;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team1983.Robot;
 import frc.team1983.utilities.motors.ControlMode;
@@ -9,7 +8,8 @@ public class SteeringDrive extends Command
 {
     protected static final double DRIVEBASE_WIDTH = 19.75 / 12;
     protected static final double MAX_VELOCITY = 14.0; // f/s
-    protected static final double TURNING_CONSTANT = 1.0;
+    protected static final double STEERING_SCALE = 2.0;
+    protected static final double VELOCITY_CORRECTION_SCALE = 2.0;
 
     private double steer, throttle = 0;
 
@@ -26,26 +26,40 @@ public class SteeringDrive extends Command
     protected void execute()
     {
         throttle = Robot.getInstance().getOI().getRightY();
+
+        // If moving in reverse, change the direction of steering
+        // This is opposite of reversing in a car
         steer = Math.signum(throttle) * Robot.getInstance().getOI().getLeftX();
 
-        double turningCorrection = 2 * (1 - Math.abs(throttle));
+        // Scale the drive train velocities based on the amount of throttle
+        // This allows the robot to maintain the same turning radius no matter the throttle
+        double velocityCorrection = VELOCITY_CORRECTION_SCALE * Math.abs(throttle);
 
-        double vl = throttle / turningCorrection, vr = throttle / turningCorrection;
+        double vl = throttle * velocityCorrection;
+        double vr = throttle * velocityCorrection;
 
+        // If steer is 0, go straight
         if(steer == 0)
         {
             Robot.getInstance().getDrivebase().set(ControlMode.Throttle, vl, vr);
             return;
         }
 
-        double r = TURNING_CONSTANT / steer - (Math.signum(steer) * TURNING_CONSTANT);
+        // Determine the turning radius
+        double r = STEERING_SCALE / steer - (STEERING_SCALE * Math.signum(steer));
 
-        vl = (throttle * (r + TURNING_CONSTANT)) / (r * Math.abs(1 - steer)) / turningCorrection;
-        vr = (throttle * (r - TURNING_CONSTANT)) / (r * Math.abs(1 - steer)) / turningCorrection;
+        // If the turning radius is 0, do a point turn
+        if(r == 0)
+        {
+            Robot.getInstance().getDrivebase().set(ControlMode.Throttle, Math.signum(steer) * vl, -Math.signum(steer) * vr);
+            return;
+        }
+
+        // Determine the wheel velocities from the radius
+        vl = (throttle * (r + STEERING_SCALE)) / (r * Math.abs(1 - steer)) * velocityCorrection;
+        vr = (throttle * (r - STEERING_SCALE)) / (r * Math.abs(1 - steer)) * velocityCorrection;
 
         Robot.getInstance().getDrivebase().set(ControlMode.Throttle, vl, vr);
-
-        System.out.println(r);
     }
 
     protected boolean isFinished()
